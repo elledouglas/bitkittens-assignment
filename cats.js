0d4e8ed9e24faf00ef8f0cb424117570b030f44d
Kathleen <kathleenmcclay@gmail.com>
2017-08-22 17:14:31 -0400

    One cat work at 100%

--- cats.js
+++ cats.js
@@ -9,16 +9,31 @@ kitten_button.addEventListener('click', function(event){
     url: 'http://bitkittens.herokuapp.com/cats.json',
     method: 'GET',
     dataType: 'json'
-  }).done(function(){
-    var details = document.querySelector('#cat1')
-    details.innerHTML = "Photo of cat name "
+  }).done(function(responseData){
+
+var catJSON = responseData
+var catList = catJSON["cats"];
+catList[0]["name"]
+    // var details = document.querySelector('.cat-box')
+    // details.innerHTML = "Photo of cat #{name "
+    //
+    var img = document.createElement('img')
+    img.src = catList[0]["photo"]
+    img.alt = " Photo of" +  catList[0]["name"]
+
+  var catDiv = document.querySelector('#cat1')
+  catDiv.append(img)[0]
+
+    //
+    // var catPic = "https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/20160731_124229.jpeg"
+    //
+    //     var img = document.createElement('img')
+    //     img.src = catPic;
 
   })
 
-    var catPic = "/finish.png"
 
-    var img = document.createElement('img')
-    img.src = catPic;
+
 
 
     // <div id="cat1" class="cat-box"></div>
